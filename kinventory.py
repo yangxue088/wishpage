a950626e4daeea90a97ccb0fd17155b3f0eac1d8
mike <yangxue088@gmail.com>
2017-01-10 20:37:18 +0800

    save

--- kinventory.py
+++ kinventory.py
@@ -27,6 +27,11 @@ def check_product_inventory(sites, asins):
                 browser.get(url)
                 logger.info('current url: ' + url)
 
+                try:
+                    price = browser.find_element_by_id('priceblock_ourprice').text
+                except:
+                    price = '-'
+
                 rank = ''
                 try:
                     rank = browser.find_element_by_xpath('''//div[@id='prodDetails']//a[contains(@href,'bestsellers')]/ancestor::td''').text
@@ -71,7 +76,8 @@ def check_product_inventory(sites, asins):
                 logger.info('inventory: ' + inventory)
                 logger.info('message: ' + message)
                 results.append(
-                    {'time': ctime, 'site': site, 'asin': asin, 'url': url, 'rank': rank, 'inventory': inventory, 'message': message})
+                    {'time': ctime, 'site': site, 'asin': asin, 'url': url, 'price': price, 'inventory': inventory, 'message': message,
+                     'rank': rank})
 
                 while True:
                     try:
@@ -86,7 +92,8 @@ def check_product_inventory(sites, asins):
                         time.sleep(1)
 
             except:
-                results.append({'time': ctime, 'site': site, 'asin': asin, 'url': url, 'rank': rank, 'inventory': 0, 'message': '-'})
+                results.append(
+                    {'time': ctime, 'site': site, 'asin': asin, 'url': url, 'price': price, 'inventory': 0, 'message': '-', 'rank': rank})
                 logger.info('product can not check inventory, url: ' + url)
 
     browser.quit()
@@ -101,11 +108,12 @@ def check_product_inventory(sites, asins):
         writer = csv.writer(file)
 
         if len(file.readlines()) == 0:
-            writer.writerow(['时间', '站点', 'ASIN', '链接', '排名', '库存', '提示'])
+            writer.writerow(['时间', '站点', 'ASIN', '链接', '价格', '库存', '提示', '排名'])
 
         for result in results:
             writer.writerow(
-                [result['time'], result['site'], result['asin'], result['url'], result['rank'], result['inventory'], result['message']])
+                [result['time'], result['site'], result['asin'], result['url'], result['price'], result['inventory'], result['message'],
+                 result['rank']])
 
     logger.info('generate inventory report finish, site: {}, asins: {}'.format(site, ', '.join(asins)))
     return results
