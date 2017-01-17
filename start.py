cfdf984268c27e6f471c3d1845180e4402bcd8f1
mike <yangxue088@gmail.com>
2017-01-17 10:34:40 +0800

    save

--- start.py
+++ start.py
@@ -10,8 +10,8 @@ import controller
 
 def start_web():
     print 'start web server...'
-    # controller.app.run(host='0.0.0.0', port=5000, threaded=True)
-    controller.app.run(host='127.0.0.1', port=5000, threaded=True)
+    controller.app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    # controller.app.run(host='127.0.0.1', port=5000, threaded=True, debug=True)
 
 
 if __name__ == "__main__":
